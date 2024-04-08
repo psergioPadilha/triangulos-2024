9af18a7bb496f358d5ba590b2d0c77dc02d1b92a
psergioPadilha <psergiopadilha@gmail.com>
2024-04-08 19:31:10 -0300

    08/04/2024

--- src/Triangulos.ConsoleApp/Program.cs
+++ src/Triangulos.ConsoleApp/Program.cs
@@ -1,10 +1,48 @@
-﻿namespace Triangulos.ConsoleApp
+﻿//Dados três valores X, Y, Z, verifiquem se eles podem ser os comprimentos dos lados de um triângulo
+//e se forem escrever uma mensagem informando se é um triângulo eqüilátero, isósceles ou escaleno
+//O comprimento de um lado do triângulo é sempre menor do que a soma dos outros dois.
+//Eqüilátero: Todos os lados iguais
+//Isósceles: Dois lados iguais
+//Escaleno: Todos os lados diferentes
+
+
+namespace Triangulos.ConsoleApp
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            while (true)
+            {
+                Triangulo novoTriangulo = new Triangulo();
+
+                Console.Clear();
+                Console.WriteLine("PROGRAMA TRIÂNGULO");
+                Console.WriteLine();
+
+                Console.Write("Lado x: ");
+                double x = double.Parse(Console.ReadLine());
+
+                Console.Write("Lado y: ");
+                double y = double.Parse(Console.ReadLine());
+
+                Console.Write("Lado z: ");
+                double z = double.Parse(Console.ReadLine());
+                Console.WriteLine();
+
+                bool triangulo = novoTriangulo.VerificaValores(x, y, z);
+
+                if (triangulo == true)
+                {
+                    string tipo = novoTriangulo.TipoDeTriangulo(x, y, z);
+
+                    Console.WriteLine("O valores correspondem a um triângulo " + tipo);
+                }
+                else
+                    Console.WriteLine("O valores 'NÃO' correspondem a valores válidos para um triângulo!");
+
+                Console.ReadLine();
+            }
         }
     }
 }
---
+++ src/Triangulos.ConsoleApp/Triangulo.cs
@@ -0,0 +1,40 @@
+﻿//Dados três valores X, Y, Z, verifiquem se eles podem ser os comprimentos dos lados de um triângulo
+//e se forem escrever uma mensagem informando se é um triângulo eqüilátero, isósceles ou escaleno
+//O comprimento de um lado do triângulo é sempre menor do que a soma dos outros dois.
+//Eqüilátero: Todos os lados iguais
+//Isósceles: Dois lados iguais
+//Escaleno: Todos os lados diferentes
+
+
+public class Triangulo
+{
+    public double ladoA;
+    public double ladoB;
+    public double ladoC;
+
+    public bool VerificaValores(double ladoA, double ladoB, double ladoC)
+    {
+        if ((ladoA + ladoB > ladoC) && (ladoA + ladoC > ladoB) && (ladoB + ladoC > ladoA))
+            return true;
+        else
+            return false;
+    }
+
+    public string TipoDeTriangulo(double ladoA, double ladoB, double ladoC)
+    {
+        string tipoDeTriangulo = "";
+
+        if ((ladoA == ladoB) && (ladoA == ladoC) && (ladoB == ladoC))
+            tipoDeTriangulo = "EQUILÁTERO";
+        else if ((ladoA == ladoB) && (ladoA != ladoC) && (ladoB != ladoC))
+            tipoDeTriangulo = "ISÓCESLES";
+        else if ((ladoA != ladoB) && (ladoA != ladoC) && (ladoB == ladoC))
+            tipoDeTriangulo = "ISÓCESLES";
+        else if ((ladoA != ladoB) && (ladoA == ladoC) && (ladoB != ladoC))
+            tipoDeTriangulo = "ISÓCESLES";
+        else if((ladoA != ladoB) && (ladoA != ladoC) && (ladoB != ladoC))
+            tipoDeTriangulo = "ESCALENO";
+
+        return tipoDeTriangulo;
+    }
+}
